326e2a21a231b6584b883b446bdd310a6227c0c8
Liubov Vorontsova <liebe83@yandex.ru>
2022-11-15 14:09:11 +0300

    change for conflict

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! New world");
     }
+    public static void main(String[] args) {
+        System.out.println("Conflict");
+    }
 }
